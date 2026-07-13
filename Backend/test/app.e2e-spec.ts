eca3a9f257a14f55ab3c89460b2177f086ac85a9
Denise Noemi Rea Diaz <105187806+DeniseRea@users.noreply.github.com>
2026-07-12 22:32:13 -0500

    fix: update import statement for supertest to use default import

--- Backend/test/app.e2e-spec.ts
+++ Backend/test/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
-import * as request from 'supertest';
+import request from 'supertest';
 import { AppModule } from '../src/app.module';
 import { PrismaService } from '../src/prisma/prisma.service';
 import mockPrismaService from '../src/prisma/__mocks__/prisma.service';
